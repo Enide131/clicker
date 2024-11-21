2f1cc03b226d968cd9efd0042f7514eb3cf84ecd
Ruslan Hrytsun <ruslan150306@gmail.com>
2024-11-21 01:31:05 +0100

    click upgrade

--- scripts/autoClicker.js
+++ scripts/autoClicker.js
@@ -1,23 +1,25 @@
- class Upgrade
+class Upgrade
  {
-    constructor(cost, increment, interval)
+    constructor(cost, increment, interval, click)
     {
       this.cost = cost;
       this.increment = increment;
       this.interval = interval;
-      this.label = `Auto Clicker (Cost: ${cost}`;
+      this.click = click;
+      this.label = `Auto Clicker (Cost: ${cost})`;
       this.count = 0;
     }
  }
  
  let autoClickers = 
     [
-      new Upgrade(10, 1, 1000),
-      new Upgrade(50, 4, 4000),
-      new Upgrade(100, 9, 10000)
+      new Upgrade(10, 1, 9999999, true),
+      new Upgrade(10, 1, 1000, false),
+      new Upgrade(50, 4, 4000, false),
+      new Upgrade(100, 9, 10000, false)
     ];
     
-    export function setupAutoClickers(container, getMoney, setMoney) 
+    export function setupAutoClickers(container, getMoney, setMoney, getClickBonus, setClickBonus) 
     {
       autoClickers.forEach((clicker) => 
       {
@@ -25,44 +27,43 @@
         autoClickerWrapper.classList.add('auto-clicker-wrapper');
     
         const autoClickButton = document.createElement('button');
-        autoClickButton.textContent = clicker.label;
         autoClickButton.classList.add('auto-click-button');
     
         const purchaseCountDisplay = document.createElement('span');
         purchaseCountDisplay.classList.add('purchase-count');
-        purchaseCountDisplay.textContent = `Bought: ${clicker.count}`;
     
-        function updateButtonState() 
+        function updateButtonStateandInformation() 
         {
           if (getMoney() >= clicker.cost) autoClickButton.disabled = false;
           else autoClickButton.disabled = true;
+          
+          autoClickButton.textContent = clicker.label;
+          purchaseCountDisplay.textContent = `Bought: ${clicker.count}`;
+          autoClickButton.textContent = `Auto Clicker (Cost: ${clicker.cost})`;
         }
-    
-        updateButtonState();
+
+        setInterval(updateButtonStateandInformation, 10);
     
         autoClickButton.addEventListener('click', () => 
         {
-          if (getMoney() >= clicker.cost) 
-          {
             setMoney(getMoney() - clicker.cost);
             clicker.count++;
             clicker.cost = Math.round(clicker.cost * 1.6);
-            purchaseCountDisplay.textContent = `Bought: ${clicker.count}`;
-            //autoClickButton.textContent = `Auto Clicker (Cost: ${clicker.cost})`;
+
+            if(clicker.click == true)
+            {
+               setClickBonus(getClickBonus() + clicker.increment);
+            }
     
             setInterval(() => 
             {
               setMoney(getMoney() + clicker.increment);
             }, clicker.interval);
           } 
-        });
-
-        setInterval(updateButtonState, 100);
+        );
     
         autoClickerWrapper.appendChild(autoClickButton);
         autoClickerWrapper.appendChild(purchaseCountDisplay);
         container.appendChild(autoClickerWrapper);
       });
-    }
-    
-  
+    } 
--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,9 @@ import { setupClickerButton } from './clicker.js';
 import { setupAutoClickers } from './autoClicker.js';
 
 let moneyCount = 0;
+let clickBonus = 1;
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
   const counterMoneyDisplay = document.getElementById('moneyCounter');
@@ -10,14 +13,18 @@ document.addEventListener('DOMContentLoaded', () => {
   const autoClickersContainer = document.getElementById('autoClickersContainer');
 
   setupClickerButton(clickerButton, () => {
-    moneyCount++;
+    moneyCount += clickBonus;
     updateMoneyCounter(counterMoneyDisplay, moneyCount);
   });
 
-  setupAutoClickers(autoClickersContainer, () => moneyCount, (newCount) => {
-    moneyCount = newCount;
-    updateMoneyCounter(counterMoneyDisplay, moneyCount);
-  });
+  setupAutoClickers(autoClickersContainer, () => moneyCount, (newCount) => 
+    {
+      moneyCount = newCount;
+      updateMoneyCounter(counterMoneyDisplay, moneyCount);
+    }, () => clickBonus, (newBonus) =>
+    {
+      clickBonus = newBonus;
+    })
 });
 
 
